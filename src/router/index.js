abeaa851f2b8040ea9a396d4332778c285adcbd4
yanglin <yanglin556@yahoo.com>
2022-09-24 22:11:33 +0800

    更新样式

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import Notebooks from '@/components/NotebookList.vue'
+import NoteDetail from '@/components/NoteDetail.vue'
+import TrashDetail from '@/components/TrashDetail.vue'
 
 Vue.use(Router)
 
@@ -10,6 +14,22 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/notebooks',
+      component: Notebooks
+    },
+    {
+      path: '/note/:noteId',
+      component: NoteDetail
+    },
+    {
+      path: '/trash/:noteId',
+      component: TrashDetail
     }
   ]
 })
